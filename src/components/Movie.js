0c896b7ed09c38e39182e8f5ca62b00936f9af9c
danielbednarz2 <danielbednarz2@gmail.com>
2021-03-15 13:56:24 -0700

    Updating date format and spacing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,18 +16,79 @@ const setVoteClass = (vote) => {
 
 const Movie = ({ title, release_date, backdrop_path, vote_average, vote_count }) => {
     const handleOnClick = () => {
-        fetch(`https://www.omdbapi.com/?apikey=${OMDB_KEY}&t="${title}"&y=${release_date}`)
+        fetch(`https://www.omdbapi.com/?apikey=${OMDB_KEY}&t="${title}"&y=${release_date.slice(0,4)}`)
         .then(res => res.json())
         .then(data => window.open(`https://www.imdb.com/title/${data.imdbID}`))
         .catch(err => console.log(err.message))
     }
+
+    let month = release_date.slice(5,7);
+    let day = release_date.slice(8,10);
+    let year = release_date.slice(0,4);
+
+    // Translate year number to word
+
+    switch (month) {
+        case '01':
+            month = 'Jan'
+            break;
+        case '02':
+            month = 'Feb'
+            break;
+        case '03':
+            month = 'March'
+            break;
+        case '04':
+            month = 'April'
+            break;
+        case '05':
+            month = 'May'
+            break;
+        case '06':
+            month = 'June'
+            break;
+        case '07':
+            month = 'July'
+            break;
+        case '08':
+            month = 'Aug'
+            break;
+        case '09':
+            month = 'Sep'
+            break;
+        case '10':
+            month = 'Oct'
+            break;
+        case '11':
+            month = 'Nov'
+            break;
+        case '12':
+            month = 'Dec'
+            break;
+        default:
+            month = 'Month'
+            break;
+    }
+
+    // Shorten the days
+
+    if (day[0] === '0') {
+        day = day.slice(1,2)
+    }
+
 
     return (
         <div className="movie" onClick={handleOnClick}>
             <img src={backdrop_path ? IMAGES_API + backdrop_path : 'https://images.unsplash.com/photo-1458053688450-eef5d21d43b3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=752&q=80'} alt={title} />
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{release_date}</span>
+                <span>
+                    {`
+                        ${month} 
+                        ${day}, 
+                        ${year}
+                    `}
+                </span>
             </div>
 
             <div className="movie-over">
